a62182972197f93ac9cca36833b6f93954fff4d5
bishkar <mvolodymyr751@gmail.com>
2024-06-29 20:23:36 +0300

    added dist

--- src/components/Info/Galery/Galery.tsx
+++ src/components/Info/Galery/Galery.tsx
@@ -8,25 +8,17 @@ function Galery({children, images}: GaleryProps) {
       <div className='my-20 flex justify-center'>
         {children}
       </div>
-      <div className="grid grid-cols-4 gap-2">
+      <div className="grid sm:grid-cols-4 grid-cols-2 gap-2">
         <div className='col-span-2'>
           <img src={images[0].src} alt={images[0].alt}/>
         </div>
         <div className='grid grid-rows-2 gap-2'>
-          <div>
-            <img src={images[1].src} alt={images[1].alt}/>
-          </div>
-          <div>
-            <img src={images[2].src} alt={images[2].alt}/>
-          </div>
+          <img src={images[1].src} alt={images[1].alt}/>
+          <img src={images[2].src} alt={images[2].alt}/>
         </div>
         <div className='grid grid-rows-2 gap-2'>
-          <div>
-            <img src={images[3].src} alt={images[3].alt}/>
-          </div>
-          <div>
-            <img src={images[4].src} alt={images[4].alt}/>
-          </div>
+          <img src={images[3].src} alt={images[3].alt}/>
+          <img src={images[4].src} alt={images[4].alt}/>
         </div>
       </div>
     </>
--- src/components/Info/InfoContainer/InfoContainer.tsx
+++ src/components/Info/InfoContainer/InfoContainer.tsx
@@ -4,7 +4,7 @@ import { InfoContainerProps } from './InfoContainer.props';
 
 function InfoContainer({ children }: InfoContainerProps) {
   return (
-    <div className='bg-black-90 sm:px-36 px-5 py-12 sm:my-10'>
+    <div className='bg-black-90 sm:px-36 px-5 py-12 sm:my-10 my-0'>
       {children}
     </div>
   );
